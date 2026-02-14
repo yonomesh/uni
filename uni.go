ffef5510c17c215c675e5407bc312350519d8a6b
skrik2 <skrik2@outlook.com>
2026-02-14 10:43:40 +0800

    feat(uni): init Config struct

---
+++ uni.go
@@ -0,0 +1,22 @@
+package uni
+
+// Config is the top (or beginning) of the Uni configuration structure.
+//
+// Many parts of this config are extensible through the use of Kaze modules.
+// Fields which have a json.RawMessage type and which appear as dots (•••) in
+// the online docs can be fulfilled by modules in a certain module
+// namespace. The docs show which modules can be used in a given place.
+//
+// Whenever a module is used, its name must be given either inline as part of
+// the module, or as the key to the module's value. The docs will make it clear
+// which to use.
+//
+// Generally, all config settings are optional, as it is Kaze convention to
+// have good, documented default values. If a parameter is required, the docs
+// should say so.
+//
+// Go programs which are directly building a Config struct value should take
+// care to populate the JSON-encodable fields of the struct (i.e. the fields
+// with `json` struct tags) if employing the module lifecycle (e.g. Provision
+// method calls).
+type Config struct{}
