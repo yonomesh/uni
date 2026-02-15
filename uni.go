a434462d75d0ecc1123f4e5a8c679388648aa45b
skrik2 <skrik2@outlook.com>
2026-02-15 12:45:07 +0800

    feat(uni): add Event struct

--- uni.go
+++ uni.go
@@ -1,5 +1,11 @@
 package uni
 
+import (
+	"time"
+
+	"github.com/yonomesh/uuid"
+)
+
 // Config is the top (or beginning) of the Uni configuration structure.
 //
 // Many parts of this config are extensible through the use of Kaze modules.
@@ -21,10 +27,43 @@ package uni
 // method calls).
 type Config struct {
 	apps map[string]App
+
+	// failedApps is a map of apps that failed to provision with their underlying error.
+	failedApps   map[string]error
+	eventEmitter eventEmitter
 }
 
 // App is a thing that Caddy runs.
 type App interface {
 	Start() error
 	Stop() error
+}
+
+// Event represents something that has happened or is happening.
+// An Event value is not synchronized, so it should be copied if
+// being used in goroutines.
+//
+// EXPERIMENTAL: Events are subject to change.
+type Event struct {
+	// If non-nil, the event has been aborted, meaning
+	// propagation has stopped to other handlers and
+	// the code should stop what it was doing. Emitters
+	// may choose to use this as a signal to adjust their
+	// code path appropriately.
+	Aborted error
+
+	// The data associated with the event. Usually the
+	// original emitter will be the only one to set or
+	// change these values, but the field is exported
+	// so handlers can have full access if needed.
+	// However, this map is not synchronized, so
+	// handlers must not use this map directly in new
+	// goroutines; instead, copy the map to use it in a
+	// goroutine. Data may be nil.
+	Data map[string]any
+
+	id     uuid.UUID
+	ts     time.Time
+	name   string
+	origin Module
 }
