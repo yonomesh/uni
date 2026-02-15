96a4f7d6026a686b6f0692c2cebc6db330c9b8a7
skrik2 <skrik2@outlook.com>
2026-02-15 20:54:19 +0800

    modules(standard): register module by importing pkg

---
+++ modules/standard/standard.go
@@ -0,0 +1,8 @@
+// Package standard imports all the standard Uni modules.
+//
+// This file exists purely to register modules via their init() functions.
+package standard
+
+import (
+	_ "github.com/yonomesh/uni/modules/demo"
+)
