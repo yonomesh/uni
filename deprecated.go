02c1939cfa965825a4380a32ba47d740537a448f
skrik2 <skrik2@outlook.com>
2026-02-15 20:06:10 +0800

    logging(refine): move some to deprecated.go

--- deprecated.go
+++ deprecated.go
@@ -13,3 +13,21 @@ import (
 type ProxyFuncProducer interface {
 	ProxyFunc() func(*http.Request) (*url.URL, error)
 }
+
+// Log represents the log data format.
+type LogEntry struct {
+	Time     string   `json:"ts"`       // Timestamp of the log entry
+	Level    string   `json:"level"`    // Log level (e.g., Trace, Debug, Info, Warning, Error, Fataland Panic)
+	Category string   `json:"category"` // Category or type of the log (e.g., user-action)
+	Tags     []string `json:"tags"`     // Tags related to the log
+	Msg      Msger    `json:"msg"`      // Msg content, implemented via the interface for customization
+	Extra    Extra    `json:"extra"`    // Extra content, implemented via the interface for customization
+}
+
+type Msger interface {
+	MsgToString() (string, error)
+}
+
+type Extra interface {
+	ExtraToString() (string, error)
+}
